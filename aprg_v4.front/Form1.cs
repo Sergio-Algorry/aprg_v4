6b35c1a610629c07a65d24a87f64bf61b72efda2
Sergio Algorry <40408779+Sergio-Algorry@users.noreply.github.com>
2025-09-02 16:50:53 -0300

    Agregar archivos de proyecto.

---
+++ aprg_v4.front/Form1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace aprg_v4.front
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
